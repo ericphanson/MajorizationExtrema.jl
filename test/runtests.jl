b6b6ee7a2fb23565be88d3e512d24e9aacf3bc97
Eric <5846501+ericphanson@users.noreply.github.com>
2019-05-24 00:23:02 +0100

    Add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,21 +12,62 @@ end
 
 @testset "Basic checks" begin
 
-    p = [1//3,1//3,1//3]
-    q = [1//4,1//4,1//2]
+    p = [1//3, 1//3, 1//3]
+    q = [1//4, 1//4, 1//2]
 
     @test p ≺ q
     @test TV(p, q) == 1//6
 
     @test majmin(q, 1//20) == [11//40, 11//40, 9//20]
-
     @test majmax(q, 1//20) == [1//5, 1//4, 11//20]
-
     @test majmax(q, 2) == [0//1, 0//1, 1//1]
-
     @test majmin(q, 2) == [1//3, 1//3, 1//3]
 
+    p = float.(p)
+    q = float.(q)
 
+    @test p ≺ q
+    @test majmin(q, 1/20) ≈ [11/40, 11/40, 9/20]
+    @test majmax(q, 1/20) ≈ [1/5, 1/4, 11/20]
+    @test majmax(q, 2) ≈ [0, 0, 1]
+
+
+end
+
+@testset "Tests with type $T" for T = [Float64, Rational{Int}, Rational{BigInt}] 
+    for d in [2, 3, 5, 10]
+    
+        δ = zeros(T, d)
+        δ[1] = one(T)
+
+        u = ones(T,d)/T(d)
+
+        for ϵ in [T(1//30), T(1//20), T(1//4)]
+            if T == Float64
+                r = randsimplexpt(d)
+            elseif T == Rational{Int}
+                r = randsimplexpt(d, rand(10:10000))
+            elseif T == Rational{BigInt}
+                r = randsimplexpt(d, big(rand(10:10000)))
+            end
+
+            r_max = majmax(r, ϵ)
+            r_min = majmin(r, ϵ)
+
+            check_simplexpt(r)
+            check_simplexpt(r_max)
+            check_simplexpt(r_min)
+
+            @test eltype(r) == T
+            @test eltype(r_max) == T
+            @test eltype(r_min) == T
+
+            @test r ≺ δ
+            @test r ≺ r_max
+            @test r_min ≺ r
+            @test u ≺ r
+        end
+    end
 end
 
 @testset "Random tests" begin
